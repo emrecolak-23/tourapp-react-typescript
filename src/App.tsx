4fc9b0ef63949150ade7a991fe288310a692eb9b
Emre Çolak <emrecolak@Emre.home>
2023-04-09 00:18:29 +0300

    create grid layout based on fluid layoutprinciple

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,37 @@ import Header from "./components/header/header.component"
 const App = () => {
   return <div>
     <Header />
+    
+    {/* <section className="grid-test">
+      <div className="row">
+        <div className="col-1-of-2">Col 1 of 2</div>
+        <div className="col-1-of-2">Col 1 of 2</div>
+      </div>
+      <div className="row">
+        <div className="col-1-of-3">Col 1 of 3</div>
+        <div className="col-1-of-3">Col 1 of 3</div>
+        <div className="col-1-of-3">Col 1 of 3</div>
+      </div>
+      <div className="row">
+        <div className="col-1-of-3">Col 1 of 3</div>
+        <div className="col-2-of-3">Col 2 of 3</div>
+      </div>
+      <div className="row">
+        <div className="col-1-of-4">Col 1 of 4</div>
+        <div className="col-1-of-4">Col 1 of 4</div>
+        <div className="col-1-of-4">Col 1 of 4</div>
+        <div className="col-1-of-4">Col 1 of 4</div>
+      </div>
+      <div className="row">
+        <div className="col-1-of-4">Col 1 of 4</div>
+        <div className="col-1-of-4">Col 1 of 4</div>
+        <div className="col-2-of-4">Col 2 of 4</div>
+      </div>
+      <div className="row">
+        <div className="col-1-of-4">Col 1 of 4</div>
+        <div className="col-3-of-4">Col 3 of 4</div>
+      </div>
+    </section> */}
   </div>
 }
 
--- src/components/header-text-box-title/header-text-box-title.component.tsx
+++ src/components/header-text-box-title/header-text-box-title.component.tsx
@@ -1,7 +1,7 @@
 import './header-text-box-title.styles.scss'
 const HeaderTextBoxTitle = () => {
     return (
-        <h1 className='heading-primary'>
+    <h1 className='heading-primary'>
         <span className='heading-primary--main'>Outdoors</span>
         <span className='heading-primary--sub'>is where life happens</span>
     </h1>
