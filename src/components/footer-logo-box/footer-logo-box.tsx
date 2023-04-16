f69bd6e0bd4d960be37020ce006d027b845862a2
Emre Çolak <emrecolak@Emre.home>
2023-04-16 22:46:22 +0300

    change images to responsive images with html

--- src/components/footer-logo-box/footer-logo-box.tsx
+++ src/components/footer-logo-box/footer-logo-box.tsx
@@ -1,10 +1,16 @@
 import './footer-logo-box.styles.scss'
-import Logo from '../../assets/img/logo-green-2x.png'
+import Logo2x from '../../assets/img/logo-green-2x.png'
+import Logo1x from '../../assets/img/logo-green-1x.png'
+import LogoSmall1x from '../../assets/img/logo-green-small-1x.png'
+import LogoSmall2x from '../../assets/img/logo-green-small-2x.png'
 
 const FooterLogoBox = () => {
     return (
     <div className="footer__logo-box">
-        <img src={Logo} alt="Full Logo" className='footer__logo' />
+        <picture className='footer__logo'>
+            <source srcSet={`${LogoSmall1x} 1x, ${LogoSmall2x} 2x`} media='(max-width: 37.5em)' />
+            <img srcSet={`${Logo1x} 1x, ${Logo2x} 2x`} alt="Full Logo" className='footer__logo' src={Logo2x} />
+        </picture>
     </div>
     )
 }
--- src/components/image-composition/image-composition.component.tsx
+++ src/components/image-composition/image-composition.component.tsx
@@ -2,13 +2,37 @@ import photo1 from '../../assets/img/nat-1-large.jpg'
 import photo2 from '../../assets/img/nat-2-large.jpg'
 import photo3 from '../../assets/img/nat-3-large.jpg'
 
+import photo1Normal from '../../assets/img/nat-1.jpg'
+import photo2Normal from '../../assets/img/nat-2.jpg'
+import photo3Normal from '../../assets/img/nat-3.jpg'
+
 import './image-composition.styles.scss'
 
 const ImageComposition = () => {
     return <div className="composition">
-        <img src={photo1} alt="Photo 1" className="composition__photo composition__photo--p1" />
+
+        <img 
+        srcSet={`${photo1Normal} 300w, ${photo1} 1000w`} 
+        sizes='(max-width: 900x) 20vw, (max-width: 600px) 30vw, 300px'
+        className="composition__photo composition__photo--p1"
+        src={photo1}
+        />
+         <img 
+        srcSet={`${photo2Normal} 300w, ${photo2} 1000w`} 
+        sizes='(max-width: 900x) 20vw, (max-width: 600px) 30vw, 300px'
+        className="composition__photo composition__photo--p2"
+        src={photo2}
+        />
+         <img 
+        srcSet={`${photo3Normal} 300w, ${photo3} 1000w`} 
+        sizes='(max-width: 900x) 20vw, (max-width: 600px) 30vw, 300px'
+        className="composition__photo composition__photo--p2"
+        src={photo3}
+        />
+
+        {/* <img src={photo1} alt="Photo 1" className="composition__photo composition__photo--p1" />
         <img src={photo2} alt="Photo 2" className="composition__photo composition__photo--p2" />
-        <img src={photo3} alt="Photo 3" className="composition__photo composition__photo--p3" />
+        <img src={photo3} alt="Photo 3" className="composition__photo composition__photo--p3" /> */}
     </div>
 }
 
